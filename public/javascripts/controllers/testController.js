bdf31492dfda450700e0cb2be64b44e5489941e6
Jon Araquistain <jonathan.araquistain@socialinsoma.com>
2014-06-15 01:47:03 -0700

    progress!

--- public/javascripts/controllers/testController.js
+++ public/javascripts/controllers/testController.js
@@ -4,7 +4,17 @@ AW.namespace('AW.Controllers.Test');
         $scope.guests = [];
         $scope.setGuests = function(guestArray) {
             $scope.guests = guestArray;
+            console.log('guests:', $scope.guests);
         };
+
+        $scope.confirmedFilter = {
+            'confirmed': true
+        };
+
+        $scope.notConfirmedFilter = {
+            'confirmed': false
+        };
+
     };
     namespace.controller.$inject = ['$scope'];
 })(AW.Controllers.Test);
