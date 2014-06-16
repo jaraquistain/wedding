8026b3188b39d4d36169f5438606787b27c4187b
Jon Araquistain <jonathan.araquistain@socialinsoma.com>
2014-06-15 17:04:48 -0700

    committing some stuff

--- public/javascripts/controllers/testController.js
+++ public/javascripts/controllers/testController.js
@@ -1,11 +1,28 @@
 AW.namespace('AW.Controllers.Test');
-(function(namespace) {
-    namespace.controller = function($scope) {
+(function (namespace) {
+    namespace.controller = function ($scope, $http) {
         $scope.guests = [];
-        $scope.setGuests = function(guestArray) {
+        $scope.setGuests = function (guestArray) {
             $scope.guests = guestArray;
             console.log('guests:', $scope.guests);
         };
+
+        $scope.guestTemplate = {
+            'firstName': '',
+            'lastName':  '',
+            'address':   {
+                'street1': '',
+                'street2': '',
+                'city':    '',
+                'state':   '',
+                'zip':     ''
+            },
+            'email':     '',
+            'phone':     '',
+            'confirmed': false
+        };
+
+        $scope.newGuest = angular.extend({}, $scope.guestTemplate);
 
         $scope.confirmedFilter = {
             'confirmed': true
@@ -15,6 +32,13 @@ AW.namespace('AW.Controllers.Test');
             'confirmed': false
         };
 
+        $scope.addNewGuest = function(){
+            $http.post('/guests.json', $scope.newTodo).success(function(response){
+                $scope.guests = response.guests;
+                $scope.newGuest = angular.extend({}, $scope.guestTemplate);
+            });
+        };
+
     };
-    namespace.controller.$inject = ['$scope'];
+    namespace.controller.$inject = ['$scope', '$http'];
 })(AW.Controllers.Test);
